3b68f2c3e6836d65e9fdfdb61879f66e621f8cb8
LuisDavid01 <luisdavidmirandav12@gmail.com>
2024-11-20 15:28:14 -0600

    tareas vista parcial, faltan crud para proyectos

---
+++ Controllers/TareasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Practica2Grupo5.Models;
+
+namespace Practica2Grupo5.Controllers
+{
+    public class TareasController : Controller
+    {
+        private Practica2Grupo5Context db = new Practica2Grupo5Context();
+
+        // GET: Tareas
+        public ActionResult Index()
+        {
+            var tareas = db.Tareas.Include(t => t.Lista);
+            return View(tareas.ToList());
+        }
+
+        // GET: Tareas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tareas.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarea);
+        }
+
+        // GET: Tareas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdLista = new SelectList(db.Listas, "IdLista", "Nombre");
+            return View();
+        }
+
+        // POST: Tareas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdTarea,Nombre,Descripcion,Completada,FechaCreacion,FechaLimite,IdLista")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tareas.Add(tarea);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdLista = new SelectList(db.Listas, "IdLista", "Nombre", tarea.IdLista);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tareas.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdLista = new SelectList(db.Listas, "IdLista", "Nombre", tarea.IdLista);
+            return View(tarea);
+        }
+
+        // POST: Tareas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdTarea,Nombre,Descripcion,Completada,FechaCreacion,FechaLimite,IdLista")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarea).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdLista = new SelectList(db.Listas, "IdLista", "Nombre", tarea.IdLista);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tareas.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarea);
+        }
+
+        // POST: Tareas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tarea tarea = db.Tareas.Find(id);
+            db.Tareas.Remove(tarea);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
