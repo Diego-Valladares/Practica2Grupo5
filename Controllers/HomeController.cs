af22cecb43dfd144d069e34b87411ff52884ae67
Reynaldo Solano Vega <114126394+ReyNaldo1904@users.noreply.github.com>
2024-11-20 18:50:41 -0900

    listas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,52 +1,132 @@
-﻿using Practica2Grupo5.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Practica2Grupo5.Models;
 
 namespace Practica2Grupo5.Controllers
 {
     public class HomeController : Controller
     {
-
         private Practica2Grupo5Context db = new Practica2Grupo5Context();
 
-        public ActionResult Test()
-        {
-            // Para probar si se ven las listas
-            var listas = db.Listas.ToList();
-            return View(listas);
-        }
-
-        public ActionResult DetailsLista() 
-        {
-            // Para probar la si se puedne ver las tareas dentro de cada lista
-            var tareas = db.Tareas.ToList();
-            return View(tareas);
-
-        }
-
-
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var tareas = db.Tareas.Include(t => t.Lista);
+            return View(tareas.ToList());
         }
 
-        public ActionResult About()
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tareas.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarea);
         }
 
-        public ActionResult Contact()
+        // GET: Home/Create
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.IdLista = new SelectList(db.Listas, "IdLista", "Nombre");
             return View();
         }
 
+        // POST: Home/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdTarea,Nombre,Descripcion,Completada,FechaCreacion,FechaLimite,IdLista")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tareas.Add(tarea);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
+            ViewBag.IdLista = new SelectList(db.Listas, "IdLista", "Nombre", tarea.IdLista);
+            return View(tarea);
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tareas.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdLista = new SelectList(db.Listas, "IdLista", "Nombre", tarea.IdLista);
+            return View(tarea);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdTarea,Nombre,Descripcion,Completada,FechaCreacion,FechaLimite,IdLista")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarea).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdLista = new SelectList(db.Listas, "IdLista", "Nombre", tarea.IdLista);
+            return View(tarea);
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tareas.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarea);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tarea tarea = db.Tareas.Find(id);
+            db.Tareas.Remove(tarea);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
